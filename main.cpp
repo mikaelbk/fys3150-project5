393e5c247d093ed32c9be77bc865d87ef92e5277
Mikael B. Kiste <mikael.kiste@gmail.com>
2017-11-30 11:16:13 +0100

    transactions with self not allowed in main.cpp. Money is conserved!

--- main.cpp
+++ main.cpp
@@ -20,32 +20,39 @@ int main(int argc, char const *argv[])
 	std::uniform_real_distribution<double> rand(0.0,1.0);
 
 	// "Initial conditions"
-	double input = atof(argv[1]);
 	int N = 500;
-	int transactions = int(pow(10,7));
-	int mcc = int(pow(20,1));
+	int transactions = int(pow(10,6));
+	int mcc = atoi(argv[1]);
+	//int mcc = int(pow(20,1));
 	double m0 = 2.0;
 
 	// other declerations
 	mat agents(N,mcc);
 	agents.col(0) = m0*ones<vec>(N);
 
-	for (int i = 1; i < mcc; ++i)
-	{	
+	// 'montecarlo loop'
+	for (int i = 1; i < mcc; ++i){	
 		agents.col(i) = agents.col(i-1);
 		cout << "mcc#\t" << i << endl;
-		for (int j = 0; j < transactions; ++j)
-		{
+
+		// perform transactions
+		double j = 0;
+		while (j < transactions){
 			int k = (int)(N*rand(gen));
 			int l = (int)(N*rand(gen));
-			double epsilon = rand(gen);
+			if(k == l){continue;}
 			double sum = agents(k,i) + agents(l, i);
-			agents(k,i) = epsilon*sum;
-			agents(l,i) = (1-epsilon)*sum;
+			agents(k,i) = rand(gen)*sum;
+			agents(l,i) = sum-agents(k,i);
+			j++;
 		}
 	}
-	string filename = to_string(mcc) + "_" + to_string(int(log10(transactions)));
-	agents.save(filename,csv_ascii);
+
+	cout << endl << sum(agents,0) << endl;
+
+	// write to file
+	//string filename = to_string(mcc) + "_" + to_string(int(log10(transactions)));
+	//agents.save(filename,csv_ascii);
 
 	return 0;
 }
