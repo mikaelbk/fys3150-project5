e1b3c4d508f82112bc5ff695ca3b3d5ee00cd4bf
Mikael B. Kiste <mikael.kiste@gmail.com>
2017-11-30 18:41:18 +0100

    reproduction of patricia et al almost complete

--- main.cpp
+++ main.cpp
@@ -18,9 +18,10 @@ int main(int argc, char const *argv[])
 	// "Initial conditions"
 	int N = 500;
 	int transactions = int(pow(10,6));
-	int mcc = atoi(argv[1]);
-	int saving = 0;
-	double m0 = 2.0;
+	int mcc = 100;
+	double saving = atof(argv[1]);
+	double m0 = atof(argv[2]);
+	string filename = argv[3];
 
 	// other declerations
 	mat agents(N,mcc);
@@ -30,7 +31,7 @@ int main(int argc, char const *argv[])
 	matrixTransactions(N, mcc, transactions, saving, agents);
 
 	// write to file
-	string filename = to_string(mcc) + "_" + to_string(int(log10(transactions)));
+	//string filename = to_string(saving) + "l_" + to_string(m0) + "m";
 	agents.save(filename,csv_ascii);
 
 	return 0;
@@ -42,10 +43,12 @@ void matrixTransactions(int N, int mcc, int transactions, double saving, mat& ag
 	std::mt19937_64 gen(rd());
 	std::uniform_real_distribution<double> rand(0.0,1.0);
 
+	// progress bar
+	cout << "0" << "%             100" << "%" << endl;
+
 	// 'montecarlo loop' 
 	for (int i = 1; i < mcc; ++i){	
 		agents.col(i) = agents.col(i-1);
-		cout << "mcc#\t" << i << endl;
 		// perform transactions
 		double j = 0;
 		while (j < transactions){
@@ -58,7 +61,8 @@ void matrixTransactions(int N, int mcc, int transactions, double saving, mat& ag
 			agents(l,i) -= dm;
 			j++;
 		}
-	}
+		if(i%(int(mcc/20.0)) == 0){cout<<"="<<std::flush;}
+	} cout << endl;
 }
 
 void writeArraysToFile(mat arrays, string labels[], int N, int mcc){
--- plotResults.py
+++ plotResults.py
@@ -1,26 +1,69 @@
 from numpy import *
 from matplotlib.pyplot import *
 import csv
+from scipy.special import gamma
 
-with open('100_6',"r") as infile:
-	lines = infile.readlines()
-	m = len(lines) # mcc
-	n = len(lines[0].split(",")) # number of agents
-	matrix = zeros([m,n])
-	for i in range(m):
-		for j in range(n):
-			matrix[i][j] = (lines[i].split(","))[j]
+def getMatrix(filename):
+	with open(filename,"r") as infile:
+		lines = infile.readlines()
+		m = len(lines) # mcc
+		n = len(lines[0].split(",")) # number of agents
+		matrix = zeros([m,n])
+		for i in range(m):
+			for j in range(n):
+				matrix[i][j] = (lines[i].split(","))[j]
+		return matrix
+
+def avgValsHist(matrix, exclusions, bins): #not yet finished
+	average = (matrix[::,exclusions::]).flatten()
+	if(len(bins)==1):
+		bins = arange(0,max(average),bins[0])
+	return 0
+
+def fitting(x,lam):
+	n = 1 + 3*lam/(1-lam)
+	an = n**n/gamma(n)
+	return an*x**(n-1)*exp(-n*x)
+
+matrix = getMatrix("0.00l.txt")
 
 # 5a
+excld = 1
 dm = 0.05
-average = (matrix[::,1:]).flatten()
-bins = arange(0,max(average),dm)
+average = (matrix[::,excld::]).flatten()
+bins = arange(0,max(average),dm,)
 b = 1/mean(average)
-
+"""
 hist(average, bins = bins , normed = 1)
 plot(bins,b*exp(-b*bins))
 show()
 
 # 5b
-#plot(sort(agentsFinal),log10(b*exp(-b*sort(agentsFinal))))
-#show()
+x = sort(average)
+y = log10(b*exp(-b*x))
+linear = polyfit(x,y,1)
+plot(x,y,'.')
+plot(x,polyval(linear,x),'r',label = 'f(x)=%.2fx%.2f'%(linear[0],linear[1]))
+legend()
+show()
+"""
+
+#5c
+matrix2 = getMatrix("0.25l.txt")
+matrix3 = getMatrix("0.50l.txt")
+matrix4 = getMatrix("0.75l.txt")
+matrix5 = getMatrix("0.90l.txt")
+y1 = histogram(average, bins = bins , normed = 1)[0]
+y2 = histogram(matrix2[::,excld::], bins = bins , normed = 1)[0]
+y3 = histogram(matrix3[::,excld::], bins = bins , normed = 1)[0]
+y4 = histogram(matrix4[::,excld::], bins = bins , normed = 1)[0]
+y5 = histogram(matrix5[::,excld::], bins = bins , normed = 1)[0]
+x = bins[0:-1]
+plot(x,b*exp(-b*x), label = "gibbs distribution")
+plot(x,y1, '.',label = "l=0")
+plot(x,y2, '.',label = "l=0.25")
+plot(x,y3, '.',label = "l=0.50")
+plot(x,y4, '.',label = "l=0.75")
+plot(x,y4, '.',label = "l=0.90")
+legend()
+show()
